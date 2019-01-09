8171844878227033a610d7dce83395ead6d10bce
김평열 <yeolpyeong@gmail.com>
2019-01-09 18:03:08 +0900

    Merciful Algospot

---
+++ MERCY.java
@@ -0,0 +1,18 @@
+/*
+ * Merciful Algospot
+ * https://algospot.com/judge/problem/read/MERCY
+ */
+
+package algospot;
+
+import java.util.Scanner;
+
+public class MERCY {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		while (N-- > 0) {
+			System.out.println("Hello Algospot!");
+		}
+	}
+}
