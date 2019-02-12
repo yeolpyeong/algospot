780ce8085c34c6e4744181fa0be722aa7f8acfdf
김평열 <yeolpyeong@gmail.com>
2019-02-12 21:14:43 +0900

    고대어 사전

--- DICTIONARY.java
+++ DICTIONARY.java
@@ -3,8 +3,6 @@
  * https://algospot.com/judge/problem/read/DICTIONARY
  */
 
-package algospot;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,6 +10,31 @@ import java.io.InputStreamReader;
 public class DICTIONARY {
 	static String dictionary, alphabet = "abcdefghijklmnopqrstuvwxyz";
 	static boolean[][] adj;
+
+	public static void generateAdj(String[] words) {
+		adj = new boolean[26][26];
+		for (int i = 0; i < words.length - 1; i++) {
+			int min = Math.min(words[i].length(), words[i + 1].length());
+			for (int pos = 0; pos < min; pos++) {
+				int a = words[i].charAt(pos) - 'a';
+				int b = words[i + 1].charAt(pos) - 'a';
+				if (a != b) {
+					adj[a][b] = true;
+					break;
+				}
+			}
+		}
+	}
+
+	public static void dfs(int i, boolean[] visited) {
+		visited[i] = true;
+		for (int j = 0; j < 26; j++) {
+			if (adj[i][j] && !visited[j]) {
+				dfs(j, visited);
+			}
+		}
+		dictionary += alphabet.charAt(i);
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -43,30 +66,5 @@ public class DICTIONARY {
 
 			System.out.println(new StringBuffer(dictionary).reverse());
 		}
-	}
-
-	public static void generateAdj(String[] words) {
-		adj = new boolean[26][26];
-		for (int i = 0; i < words.length - 1; i++) {
-			int min = Math.min(words[i].length(), words[i + 1].length());
-			for (int pos = 0; pos < min; pos++) {
-				int a = words[i].charAt(pos) - 'a';
-				int b = words[i + 1].charAt(pos) - 'a';
-				if (a != b) {
-					adj[a][b] = true;
-					break;
-				}
-			}
-		}
-	}
-
-	public static void dfs(int i, boolean[] visited) {
-		visited[i] = true;
-		for (int j = 0; j < 26; j++) {
-			if (adj[i][j] && !visited[j]) {
-				dfs(j, visited);
-			}
-		}
-		dictionary += alphabet.charAt(i);
 	}
 }
